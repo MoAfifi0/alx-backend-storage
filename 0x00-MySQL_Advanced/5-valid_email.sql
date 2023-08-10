2858b57207df340da1e2403cd6b896a837749ab0
MoAfifi0 <m.afifi00000gmail.com>
2023-08-10 11:10:29 +0200

    MySQL Advanced

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Creates a trigger that resets the attribute valid_email
+
+DROP TRIGGER IF EXISTS email_checker;
+DELIMITER $$
+CREATE TRIGGER email_checker
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END $$
+DELIMITER ;
